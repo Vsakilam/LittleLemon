717da155ae3f346612b506dfd47647102865cfca
Vsakilam <Vsakilam@ttu.edu>
2024-02-24 11:03:34 +0530

    final changes

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'Pramod@24',
+        'PASSWORD': 'root@123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
